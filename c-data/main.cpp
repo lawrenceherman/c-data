8a259803e9320c7a296986d153dedc2b2bad55e8
Lawrence Herman <lawrenceherman42@gmail.com>
2019-03-01 08:10:59 -0500

    first commit

---
+++ c-data/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c-data
+//
+//  Created by Lawrence Herman on 3/1/19.
+//  Copyright © 2019 Lawrence Herman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
